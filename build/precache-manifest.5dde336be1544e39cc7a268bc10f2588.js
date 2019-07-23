84ebe5469b1b6bf9236af8495f39b51a5a194162
Kaishan <kaishanpatel26@gmail.com>
2019-07-23 02:47:47 +0100

    first commit

---
+++ build/precache-manifest.5dde336be1544e39cc7a268bc10f2588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f0ed51f56fb03320e0488d077fd444",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d8d666e8685c9805a3b0",
+    "url": "/static/css/main.084f32c6.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "d8d666e8685c9805a3b0",
+    "url": "/static/js/main.1b6c9c9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
